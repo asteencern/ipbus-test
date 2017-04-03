21bf575fe9bf3defbce44b02f751d94c39e92f21
asteencern <arnaud.steen@cern.ch>
2017-04-03 17:55:26 +0200

    modify scripts/runIpbus-hw.py so DummyHardwareTcp can also be launched

--- scripts/runIpbus-hw.py
+++ scripts/runIpbus-hw.py
@@ -6,17 +6,29 @@ def mycallback(option,opt,value,parser):
 
 parser = OptionParser()
 
-parser.add_option("-p","--port",type='string',action='callback',
-                  callback=mycallback, dest='port')
+parser.add_option("-p","--portUdp",type='string',action='callback',
+                  callback=mycallback, dest='portUdp')
+
+parser.add_option("-t","--portTcp",type='string',action='callback',
+                  callback=mycallback, dest='portTcp')
 
 (options, args) = parser.parse_args()
 print options
 
-for i in options.port:
-    startudponport="/opt/cactus/bin/uhal/tests/DummyHardwareUdp.exe -p "+i+" -v 2 &"
-    print startudponport
-    os.system(startudponport)
+print "I start to kill all DummyHardwareUdp.exe and DummyHardwareTcp.exe"
+os.system("killall DummyHardwareUdp.exe")
+os.system("killall DummyHardwareTcp.exe")
+if options.portUdp:
+    for i in options.portUdp:
+        startudponport="/opt/cactus/bin/uhal/tests/DummyHardwareUdp.exe -p "+i+" -v 2 &"
+        print startudponport
+        os.system(startudponport)
 
+if options.portTcp:
+    for i in options.portTcp:
+        startudponport="/opt/cactus/bin/uhal/tests/DummyHardwareTcp.exe -p "+i+" -v 2 &"
+        print startudponport
+        os.system(startudponport)
 
 #check port are ope: 
 #netstat -lnup | grep DummyHardware
