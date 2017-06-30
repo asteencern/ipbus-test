883ea14bbc15f3915d93c79f6f0a2325f2acc6c1
asteencern <arnaud.steen@cern.ch>
2017-06-30 16:06:22 +0200

    update runOrmBoards.py

--- scripts/runOrmBoards.py
+++ scripts/runOrmBoards.py
@@ -11,15 +11,22 @@ parser.add_option("-d","--deviceIds",type='string',action='callback',
 
 parser.add_option("-N","--nSlaves",type='int',dest='nSlaves',default=1)
 
-parser.add_option("-f","--inputfile",type='string',dest='inputfile',default="RUN_170317_0912.raw.txt")
+parser.add_option("-f","--inputfiles",type='string',action='callback',
+                  callback=mycallback, dest='inputfiles')
 
 (options, args) = parser.parse_args()
 print options
 
+if len(options.deviceIds)!=len(options.inputfiles):
+	print 'WARNING deviceIds and inputfiles parameter options don\'t have the same size -> inputfiles take default run as input'
+	options.inputfiles=[]
+	for i in range(0,len(options.deviceIds)):
+		options.inputfiles.append('RUN_170317_0912.raw.txt')
+
 os.system("killall ormemulation")
 
-for i in options.deviceIds:
-    start="\"source scripts/env.sh; ./bin/ormemulation file://./etc/orm-connection.xml "+i+" "+str(options.nSlaves)+" "+options.inputfile+"\""
+for i in range(0,len(options.deviceIds)):
+    start="\"source scripts/env.sh; ./bin/ormemulation file://./etc/orm-connection.xml "+options.deviceIds[i]+" "+str(options.nSlaves)+" "+options.inputfiles[i]+"\""
     cmd="\'bash -c "+start+" \' &"
     cmd="gnome-terminal -e "+cmd
     print cmd
